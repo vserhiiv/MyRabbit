a038f0f2be103d2bfb06d015e7923d7b55e07547
Winch <57771030+vserhiiv@users.noreply.github.com>
2025-07-15 18:35:16 +0300

    fix push issues

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -3,30 +3,47 @@ using RabbitMQ.Client.Events;
 using System.Text;
 
 var factory = new ConnectionFactory() { HostName = "localhost" };
+
 using var connection = await factory.CreateConnectionAsync();
 using var channel = await connection.CreateChannelAsync();
 
-await channel.ExchangeDeclareAsync(exchange: "mytopicgexchange", ExchangeType.Topic);
-
-var queueDeclare = await channel.QueueDeclareAsync();
-
-var queueName = queueDeclare.QueueName;
-
-await channel.QueueBindAsync(queue: queueName, exchange: "mytopicgexchange", routingKey: "*.europe.*");
+var replyQueue = await channel.QueueDeclareAsync(queue: string.Empty, exclusive: true);
+await channel.QueueDeclareAsync("request-queue", exclusive: false);
 
 var consumer = new AsyncEventingBasicConsumer(channel);
 
-consumer.ReceivedAsync += (model, ea) =>
+consumer.ReceivedAsync += async (model, ea) =>
 {
+    await Task.Delay(3000);
+
     var body = ea.Body.ToArray();
     var message = Encoding.UTF8.GetString(body);
-    Console.WriteLine($"Analytics - received new message: {message}");
-
-    return Task.CompletedTask;
+    Console.WriteLine($"Reply recieved: {message}");
 };
 
-await channel.BasicConsumeAsync(queue: queueName, autoAck: true, consumer: consumer);
+await channel.BasicConsumeAsync(queue: replyQueue.QueueName, autoAck: true, consumer: consumer);
 
-Console.WriteLine("Analytics - Consuming...");
+for (int i = 0; i < 3; i++)
+{
+    var message = "Can I request reply";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    var properties = new BasicProperties
+    {
+        CorrelationId = Guid.NewGuid().ToString(),
+        ReplyTo = replyQueue.QueueName
+    };
+
+    await channel.BasicPublishAsync(
+        exchange: string.Empty,
+        routingKey: "request-queue",
+        mandatory: true,
+        basicProperties: properties,
+        body: body);
+
+    Console.WriteLine($"Sending Request: {properties.CorrelationId}");
+
+    await Task.Delay(1000);
+}
 
 Console.ReadKey();
--- Produser/Program.cs
+++ Produser/Program.cs
@@ -1,38 +1,29 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System.Text;
 
 var factory = new ConnectionFactory() { HostName = "localhost" };
+
 using var connection = await factory.CreateConnectionAsync();
 using var channel = await connection.CreateChannelAsync();
 
-await channel.ExchangeDeclareAsync(exchange: "mytopicgexchange", type: ExchangeType.Topic);
+await channel.QueueDeclareAsync("request-queue", exclusive: false);
 
-var random = new Random();
+var consumer = new AsyncEventingBasicConsumer(channel);
 
-var messageId = 1;
-
-while (true)
+consumer.ReceivedAsync += (model, ea) =>
 {
-    var publishingTime = random.Next(1, 4);
+    Console.WriteLine($"Received Request: {ea.BasicProperties.CorrelationId}");
 
-    var userPaymentMessage = $"A european user paid for something: {messageId}";
+    var replyMessage = $"This is your reply: {ea.BasicProperties.CorrelationId}";
 
-    var userPaymentBody = Encoding.UTF8.GetBytes(userPaymentMessage);
+    var body = Encoding.UTF8.GetBytes(replyMessage);
 
-    await channel.BasicPublishAsync(exchange: "mytopicgexchange", routingKey: "user.europe.payments", body: userPaymentBody);
-    
-    Console.WriteLine($"This message needs to be routed: {userPaymentMessage}");
+    channel.BasicPublishAsync(string.Empty, ea.BasicProperties.ReplyTo!, body);
 
+    return Task.CompletedTask;
+};
 
-    var businessOrderMessage = $"A european bussiness ordered goods: {messageId}";
-    
-    var businessOrderBody = Encoding.UTF8.GetBytes(businessOrderMessage);
-    
-    await channel.BasicPublishAsync(exchange: "mytopicgexchange", routingKey: "business.europe.order", body: businessOrderBody);
-    
-    Console.WriteLine($"This message needs to be routed: {businessOrderMessage}");
+await channel.BasicConsumeAsync(queue: "request-queue", autoAck: true, consumer: consumer);
 
-    Task.Delay(TimeSpan.FromSeconds(publishingTime)).Wait();
-
-    messageId++;
-}
+Console.ReadKey();
